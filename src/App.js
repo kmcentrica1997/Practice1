bbcd181c7eb16db2d8f3715a086b3ce3b780f340
kmcentrica1997 <karthikan.murugathas@centrica.com>
2021-11-11 00:17:41 +0000

    This is a new commit for what I originally planned to be amended

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+  StyleSheet,
+  SafeAreaView,
+  Text,
+  View,
+  Platform,
+  StatusBar,
+} from 'react-native';
+import Review from './screens/Review/Review';
+
+export default function App() {
+  return <Review />;
+}
---
+++ src/screens/Review/Review.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import {View} from 'react-native';
+import {styles} from './Review.styles';
+
+export default function Review() {
+  return <View style={styles.container} />;
+}
